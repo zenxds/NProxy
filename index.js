dab039ba80dbd0756ee06c0e3c6f84e6c7670c6b
Dongshuang.xds <dongshuang.xds@alibaba-inc.com>
2017-04-01 19:25:14 +0800

    重构代码

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
 'use strict'
-
+const fs = require('fs')
 const yargs = require('yargs')
 const util = require('util')
 const ARGS = yargs.argv
 
-let Config = {}
-try {
-    Config = require('./config.json')
-} catch (e) {}
-const Package = require('./package.json')
+const localConfig = fs.existsSync('./config.json') ? require('./config.json') : {}
+const pkg = require('./package.json')
 
-let config = {
-    debug: !!ARGS.d,
-    name: Package.name,
-    version: Package.version
+const config = {
+  debug: !!ARGS.d,
+  name: pkg.name,
+  version: pkg.version
 }
 
 if (ARGS.t) {
-    require('./lib/tunnel').run(Object.assign(config, {
-        port: 1337
-    }, Config.tunnel))
+  require('./lib/tunnel').run(Object.assign(config, {
+    port: 1337
+  }, localConfig.tunnel || {}))
 }
 
 if (ARGS.c) {
-    require('./lib/client').run(Object.assign(config, {
-        port: 1113,
-        password: '',
-        // method: 'aes-256-cfb',
-        serverHost: '',
-        serverPort: 8883
-    }, Config.client))
+  require('./lib/client').run(Object.assign(config, {
+    port: 1113,
+    password: '',
+    serverHost: '',
+    serverPort: 8883
+  }, localConfig.client || {}))
 }
 
 if (ARGS.s) {
-    require('./lib/server').run(Object.assign(config, {
-        port: 8883,
-        password: '',
-        // method: 'aes-256-cfb',
-        clientHost: '127.0.0.1',
-        clientPort: 1113
-    }, Config.server))
+  require('./lib/server').run(Object.assign(config, {
+    port: 8883,
+    password: '',
+    clientHost: '127.0.0.1',
+    clientPort: 1113
+  }, localConfig.server || {}))
 }
