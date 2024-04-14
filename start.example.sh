c38d0e9738328baaef9b9bd4927ff206a4151ac5
桥林 <dongshuang.xds@alibaba-inc.com>
2024-04-14 09:54:36 +0800

    feat: 🎸 update

--- start.example.sh
+++ start.example.sh
@@ -1,5 +1,7 @@
+# docker build -f server.dockerfile -t zenxds/nproxy:latest .
 docker run --name nproxy \
   -p 8886:8886 \
+  # -p 1113:1113 \
   -v $PWD/config.js:/home/node/app/config.js \
   --restart always \
   -d zenxds/nproxy:latest
